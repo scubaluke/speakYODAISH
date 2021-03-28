fb29324765e43946ff34b32360fe2d097a4aa079
Luke Gartland <lukegartland@gmail.com>
2021-03-28 13:46:58 -1000

    https

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # speakYODAISH
 
 A fun little api call to a YODA translator.
+
+Check it out, you may, here: https://scubaluke.github.io/speakYODAISH/
--- front.js
+++ front.js
@@ -3,7 +3,7 @@ const printResponse = document.querySelector('.response')
 
 async function getYoda() {
   
-    const url = `http://yoda-api.appspot.com/api/v1/yodish?text=${encodeURIComponent(textInput.value)}`
+    const url = `https://yoda-api.appspot.com/api/v1/yodish?text=${encodeURIComponent(textInput.value)}`
     try {
         const res = await fetch(url);
         const data = await res.json()
